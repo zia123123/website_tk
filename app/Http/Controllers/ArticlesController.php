bdce2390be5266d72fff2aac2b80750395378e0e
zia123123 <ziamuhamad@gmail.com>
2022-10-05 18:05:34 +0700

    first commit

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Articles;
+use Illuminate\Http\Request;
+
+class ArticlesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Articles::all();
+        return view('articles.index', [
+            'articles' => $articles
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'judul'=>'required',
+            'content'=> 'required',
+            'filename' => 'required'
+          ]);
+          $filename = $request->file('filename');
+          $name = $filename->getClientOriginalName();
+          $dist = 'uploads/';
+          $nameExp = explode('.', $name);
+          $nameActExp = strtolower(end($nameExp));
+          $newName = uniqid('', true).'.'.$nameActExp;
+          $upload = $filename->move($dist, $newName);
+
+          $article = new Articles([
+            'judul' => $request->get('judul'),
+            'content'=> $request->get('content'),
+            'filename' => $dist.$newName
+          ]);
+          $article->save();
+        return redirect()->route('articles.index')
+            ->with('success_message', 'Berhasil menambah Data baru');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $article = Articles::find($id);
+        if (!$article) return redirect()->route('articles.index')
+            ->with('error_message', 'User dengan id '.$id.' tidak ditemukan');
+        return view('articles.edit', [
+            'article' => $article
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'judul'=>'required',
+            'content'=> 'required',
+            'filename' => 'required'
+          ]);
+
+        $filename = $request->file('filename');
+        $name = $filename->getClientOriginalName();
+        $dist = 'uploads/';
+        $nameExp = explode('.', $name);
+        $nameActExp = strtolower(end($nameExp));
+        $newName = uniqid('', true).'.'.$nameActExp;
+        $upload = $filename->move($dist, $newName);
+          $article = Articles::find($id);
+          $article->judul = $request->get('judul');
+          $article->content = $request->get('content');
+          $article->filename = $dist.$newName;
+          $article->save();
+        return redirect()->route('articles.index')
+            ->with('success_message', 'Berhasil mengubah Data');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, $id)
+    {
+        $article = Articles::find($id);
+        if ($id == $request->article()->id) return redirect()->route('articles.index')
+            ->with('error_message', 'Anda tidak dapat menghapus diri sendiri.');
+        if ($article) $article->delete();
+        return redirect()->route('articles.index')
+            ->with('success_message', 'Berhasil menghapus user');
+    }
+}
---
+++ resources/views/articles/edit.blade.php
@@ -0,0 +1,46 @@
+@extends('adminlte::page')
+
+@section('title', 'Edit User')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Edit User</h1>
+@stop
+
+@section('content')
+    <form action="{{route('articles.update', $article)}}" method="post" enctype="multipart/form-data" >
+        @method('PUT')
+        @csrf
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+
+                        <div class="form-group">
+                            <label for="exampleInputJudul">Judul</label>
+                            <input type="text" class="form-control @error('judul') is-invalid @enderror" id="exampleInputJudul" placeholder="Judul" name="judul" value="{{$article->judul ?? old('judul')}}">
+                            @error('judul') <span class="text-danger">{{$message}}</span> @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleInputContent">Content</label>
+                            <input type="text" class="form-control @error('content') is-invalid @enderror" id="exampleInputContent" placeholder="Masukkan content" name="content" value="{{$article->content ?? old('content')}}">
+                            @error('content') <span class="text-danger">{{$message}}</span> @enderror
+                        </div>
+                        <div class="form-group">
+                        <div>
+                         <input id="minute_length" type="file" name="filename" required="">
+                        </div>
+                        </div>
+
+                    </div>
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                        <a href="{{route('articles.index')}}" class="btn btn-default">
+                            Batal
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+@stop
